e4c658cca77bb16265681cd5fa0d3ee4c577adb8
Andrey Kovalenko <ak@taucode.com>
2020-01-31 00:21:05 +0200

    Virtual method.

--- src/TauCode.Mq.Autofac/AutofacMessageHandlerContextFactory.cs
+++ src/TauCode.Mq.Autofac/AutofacMessageHandlerContextFactory.cs
@@ -1,7 +1,6 @@
 ﻿using Autofac;
 using System;
 
-// todo clean
 namespace TauCode.Mq.Autofac
 {
     public class AutofacMessageHandlerContextFactory : IMessageHandlerContextFactory
@@ -13,19 +12,11 @@ namespace TauCode.Mq.Autofac
             _rootLifetimeScope = rootLifetimeScope ?? throw new ArgumentNullException(nameof(rootLifetimeScope));
         }
 
-        public IMessageHandlerContext CreateContext()
+        public virtual IMessageHandlerContext CreateContext()
         {
             var childScope = _rootLifetimeScope.BeginLifetimeScope();
             var context = new AutofacMessageHandlerContext(childScope);
             return context;
         }
-
-        //public IMessageHandler CreateHandler(IMessageHandlerContext context, Type handlerType)
-        //{
-        //    var autofacMessageHandlerContext = (AutofacMessageHandlerContext)context; // todo check this
-        //    var scope = autofacMessageHandlerContext.ContextLifetimeScope;
-        //    var handler = (IMessageHandler)scope.Resolve(handlerType);
-        //    return handler;
-        //}
     }
 }
