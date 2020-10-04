99f20f93a5e6e30e5f59d5d5bace78e8d7cca348
Andrey Kovalenko <ak@taucode.com>
2020-10-05 01:04:30 +0300

    Got rid of lab.

--- test-lab/TauCode.Lab.Mq.NHibernate.Tests.App/Client/Dto/Notes/CreateNoteCommandDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TauCode.Lab.Mq.NHibernate.Tests.App.Client.Dto.Notes
-{
-    public class CreateNoteCommandDto
-    {
-    }
-}
--- test-lab/TauCode.Lab.Mq.NHibernate.Tests.App/Core/Features/Notes/GetUserNotes/GetUserNotesQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using TauCode.Cqrs.Queries;
-
-namespace TauCode.Lab.Mq.NHibernate.Tests.App.Core.Features.Notes.GetUserNotes
-{
-    public class GetUserNotesQuery : Query<GetUserNotesQueryResult>
-    {
-        public string UserId { get; set; }
-    }
-}
